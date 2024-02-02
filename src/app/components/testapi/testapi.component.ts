ff61b347a40f22c93a4ea0d0c71ae445307242da
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2024-02-02 11:08:29 +0100

    Add components
    Ajout d'un composant

---
+++ src/app/components/testapi/testapi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testapi',
+  standalone: true,
+  imports: [],
+  templateUrl: './testapi.component.html',
+  styleUrl: './testapi.component.css'
+})
+export class TestapiComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { TestapiComponent } from './app/components/testapi/testapi.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(TestapiComponent, appConfig)
   .catch((err) => console.error(err));
