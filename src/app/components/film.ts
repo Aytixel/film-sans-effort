433fc4672883aad7c0ccdffddc86af2edaf854ab
Aytixel <aytixel@gmail.com>
2024-02-20 13:59:42 +0100

    Ajout class film

---
+++ src/app/components/film.ts
@@ -0,0 +1,11 @@
+export class Film {
+    id: string;
+    title: string;
+    image: string;
+
+    constructor(id: string, title: string, image: string) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+    }
+}
