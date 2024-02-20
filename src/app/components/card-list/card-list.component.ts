2743db70e8f9ab8bdcb7a332d0863e9793e5b064
Aytixel <aytixel@gmail.com>
2024-02-20 18:28:17 +0100

    Recherche de film fonctionnel

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CardComponent } from "../card/card.component";
 import { Film } from '../film';
-import { CommonModule, NgFor } from '@angular/common';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-card-list',
@@ -12,7 +12,5 @@ import { CommonModule, NgFor } from '@angular/common';
 })
 export class CardListComponent {
   @Input() title: string = '';
-  @Input() filmList: Film[] = [
-    new Film(0, 'Wonka', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/aKAnL3sr5iz5SsOs89vS2k1XrKO.jpg', false)
-  ];
+  @Input() filmList: Film[] = [];
 }
