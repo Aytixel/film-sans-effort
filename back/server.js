37a8ca4165ecedd87ff3c79f73d4c6dca4f6415d
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2024-02-23 14:27:55 +0100

    Connexion à mongoDB
    Mise en place de la liaison à la connexion à la bdd.
    
    Co-Authored-By: RyuAragi <122371001+RyuAragi@users.noreply.github.com>

--- back/server.js
+++ back/server.js
@@ -2,7 +2,35 @@ require("dotenv").config();
 
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+// const mdp bdd
+const passwordBDD = process.env['MDP_BDD'];
 
+// Connexion bdd
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://dbFilm:${passwordBDD}@cluster0.44yksnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+// Suite du code
 
 class MoviesService {
 
